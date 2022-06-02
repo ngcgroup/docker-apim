e8eff1cf3bd203d12d2ce8e8d343fcbb37a6b2ba
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2022-06-01 23:47:20 -0700

    routing 3

--- docker-compose/heroku/run.sh
+++ docker-compose/heroku/run.sh
@@ -11,4 +11,4 @@ done;
 IFS=$TEMPIFS;
 export DATABASE_URL=postgres://pguser:${DB_PASS}@api-db:5432/apimgmt # api database url
 export HEROKU_POSTGRESQL_YELLOW_URL=postgres://pguser:${DB_PASS}@shared-db:5432/shared  #shared database url
-docker-compose -f docker-compose.yml -f docker-compose-as-postgre.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose-as-postgre.yml up --build -d
