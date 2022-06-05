e3108694cbd996b6566d9082bae4d4d13eacdde2
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2022-06-04 19:49:52 -0700

    adjust

--- docker-compose/heroku/run.sh
+++ docker-compose/heroku/run.sh
@@ -9,7 +9,6 @@ for var in $(heroku config -a apistudio2 | grep -v "apistudio2 Config Vars"); do
 	fi; 
 done; 
 IFS=$TEMPIFS;
-DB_PASS=password123
 export DATABASE_URL=postgres://pguser:${DB_PASS}@api-db:5432/apimgmt # api database url
 export HEROKU_POSTGRESQL_YELLOW_URL=postgres://pguser:${DB_PASS}@shared-db:5432/shared  #shared database url
 docker-compose -f docker-compose.yml -f docker-compose-as-postgre.yml up --build -d
--- docker-compose/heroku/setup-docker.sh
+++ docker-compose/heroku/setup-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+set -e
+set -x
 TEMPIFS=$IFS; 
 IFS=$'\n'; 
 for var in $(heroku config -a apistudio2 | grep -v "apistudio2 Config Vars"); do 
@@ -10,8 +11,10 @@ for var in $(heroku config -a apistudio2 | grep -v "apistudio2 Config Vars"); do
 done; 
 IFS=$TEMPIFS;
 docker-compose -f docker-compose-as-postgre.yml up -d
+sleep 20
 PGPASSWORD=$DB_PASS psql -h $DOCKHERO_HOST -U pguser -p 5432 -d shared < ../../dbscripts/postgresql.sql
 PGPASSWORD=$DB_PASS psql -h $DOCKHERO_HOST -U pguser -p 6432 -d apimgmt < ../../dbscripts/apimgt/postgresql.sql
-
 docker-compose -f docker-compose-as-postgre.yml down
+export DATABASE_URL=postgres://pguser:${DB_PASS}@api-db:5432/apimgmt # api database url
+export HEROKU_POSTGRESQL_YELLOW_URL=postgres://pguser:${DB_PASS}@shared-db:5432/shared  #shared database url
 docker-compose -f docker-compose.yml -f docker-compose-as-postgre.yml up  --build -d
